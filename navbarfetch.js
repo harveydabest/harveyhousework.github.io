a894e225e85b05d5ed805a3a6b7ba4179cb7d9dd
accessgames <83598795+accessgames@users.noreply.github.com>
2023-11-26 16:42:34 -0800

    Bloat

--- navbarfetch.js
+++
@@ -1,6 +0,0 @@
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('content').innerHTML = data;
-        })
-        .catch(error => console.error(error));
